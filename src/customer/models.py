811bcbb1249bb2c68ed391df3371148a91b8ebbf
owen <tyowen@live.com>
2024-12-22 11:52:04 -0800

    feat: add customer model

--- src/customer/models.py
+++ src/customer/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+from django.db.models import Model, UUIDField, CharField, EmailField
+from uuid import uuid4
 
-# Create your models here.
+class Customer(Model):
+    id = UUIDField(primary_key=True, default=uuid4, editable=False)
+    first_name = CharField(max_length=100)
+    middle_name = CharField(max_length=100, null=True, blank=True)
+    last_name = CharField(max_length=100)
+    email = EmailField(unique=True)
+    phone_number = CharField(max_length=20, null=True, blank=True)
