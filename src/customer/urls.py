3ec5144fb582572eb9ac4ae1a448b626237e4af5
owen <tyowen@live.com>
2024-12-22 13:18:21 -0800

    feat: create endpoint urls

---
+++ src/customer/urls.py
@@ -0,0 +1,11 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .views import CustomerViewSet
+
+router = DefaultRouter()
+router.register(r"customers", CustomerViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
